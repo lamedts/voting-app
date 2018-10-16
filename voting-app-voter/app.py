0651187ef39ff7c5138b7b2f542ab78775594384
Ed Lam <lamedts@gmail.com>
2018-10-16 18:05:59 +0800

    voter done

--- voting-app-voter/app.py
+++ voting-app-voter/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, make_response, g
-from redis import Redis
 import os
 import socket
 import random
 import json
+import grpc
+
+import vote_pb2
+import vote_pb2_grpc
 
 option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
@@ -11,24 +14,23 @@ hostname = socket.gethostname()
 
 app = Flask(__name__)
 
-def get_redis():
-    if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
-    return g.redis
-
 @app.route("/", methods=['POST','GET'])
 def hello():
     voter_id = request.cookies.get('voter_id')
     if not voter_id:
-        voter_id = hex(random.getrandbits(64))[2:-1]
+        voter_id = random.randint(0, 1000)
 
     vote = None
 
     if request.method == 'POST':
-        redis = get_redis()
         vote = request.form['vote']
-        data = json.dumps({'voter_id': voter_id, 'vote': vote})
-        redis.rpush('votes', data)
+        # redis = get_redis()
+        # data = json.dumps({'voter_id': voter_id, 'vote': vote})
+        # redis.rpush('votes', data)
+        print()
+        vote_data = vote_pb2.Vote(votedID=int(voter_id), vote=vote)
+        response = stub.SetVote(vote_data)
+        print(response)
 
     resp = make_response(render_template(
         'index.html',
@@ -37,11 +39,19 @@ def hello():
         hostname=hostname,
         vote=vote,
     ))
-    resp.set_cookie('voter_id', voter_id)
+    resp.set_cookie('voter_id', str(voter_id))
+    return resp
+
+@app.route("/reset", methods=['GET'])
+def reset():
+    resp = make_response('', 204)
+    resp.delete_cookie('voter_id')
     return resp
 
 
 if __name__ == "__main__":
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = vote_pb2_grpc.VoteWorkerServiceStub(channel)
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
 
 
--- voting-app-worker/pb/vote-handle.go
+++ voting-app-worker/pb/vote-handle.go
@@ -9,21 +9,21 @@ import (
 	context "golang.org/x/net/context"
 )
 
-var appLogger *logrus.Entry = logger.GetLogger("app")
+var voteLogger *logrus.Entry = logger.GetLogger("vote")
 
 type VoteServer struct{}
 
 func (s *VoteServer) GetVotesResults(req *WorkerRequest, stream VoteWorkerService_GetVotesResultsServer) error {
 	results := datastore.PgDBInstance.GetVoteResults()
-	appLogger.Infof("%+v", results)
+	voteLogger.Infof("%+v", results)
 	for _, result := range results {
-		appLogger.Infof("%+v", result)
+		voteLogger.Infof("%+v", result)
 		tmpResop := &VoteResults{
 			Vote:  result.Vote,
 			Count: result.Count,
 		}
 		if err := stream.Send(tmpResop); err != nil {
-			appLogger.Errorf("%+v", err)
+			voteLogger.Errorf("%+v", err)
 			return err
 		}
 	}
@@ -33,7 +33,7 @@ func (s *VoteServer) GetVotesResults(req *WorkerRequest, stream VoteWorkerServic
 func (s *VoteServer) GetVotes(req *WorkerRequest, stream VoteWorkerService_GetVotesServer) error {
 	// results := datastore.PgDBInstance.GetAllVotes()
 	// for result := range results {
-	// 	appLogger.Infof("%+v", result)
+	// 	voteLogger.Infof("%+v", result)
 	// 	if err := stream.Send(nil); err != nil {
 	// 		return err
 	// 	}
@@ -52,6 +52,7 @@ func (s *VoteServer) SetVote(ctx context.Context, vote *Vote) (*VoteStatus, erro
 	} else {
 		voteStatus.Status = "ok"
 	}
+	voteLogger.Infof("SetVote %+v", voteStatus)
 	return &voteStatus, nil
 }
 
@@ -61,5 +62,6 @@ func (s *VoteServer) GetVote(ctx context.Context, vote *Vote) (*Vote, error) {
 		return nil, err
 	}
 	vote.Vote = *option
+	voteLogger.Infof("GetVote %+v", vote)
 	return vote, nil
 }
